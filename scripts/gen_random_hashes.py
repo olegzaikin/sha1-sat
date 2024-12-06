17187dcb58f8a4ce467fe564f47122be990aa67c
olegzaikin <oleg.zaikin@icc.ru>
2024-12-06 20:18:51 +0800

    Print random hashes.

--- scripts/gen_random_hashes.py
+++ scripts/gen_random_hashes.py
@@ -11,10 +11,11 @@
 import random
 
 script_name = "gen_random_hashes.py"
-version = "0.0.1"
+version = "0.0.2"
 
 random.seed(0)
 
+random_strings = []
 with open('hashes_256bit.txt', 'w') as ofile:
     ofile.write(('0'*256) + '\n')
     ofile.write(('1'*256) + '\n')
@@ -22,4 +23,13 @@ with open('hashes_256bit.txt', 'w') as ofile:
         s = ''
         for j in range(256):
             s += str(random.randint(0, 1))
+        random_strings.append(s)
         ofile.write(s + '\n')
+
+
+hash_sizes = [128, 160, 256]
+for hash_size in hash_sizes:
+    print(str(hash_size) + '-bit random messages in hex:')
+    for s in random_strings:
+        print(str(hex(int(s[:hash_size], 2))))
+    print('')
